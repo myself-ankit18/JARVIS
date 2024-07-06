d3d5a3a54d27156f925556ea7990eb798d5ea00b
Ankit Kumar Dam <aparnadam1981@gmail.com>
2024-07-06 23:44:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+from googleapiclient.discovery import build
 import socialmedia
 import music
-import wikipediaapi
 
 engine = pyttsx3.init()
 
-wiki_wiki = wikipediaapi.Wikipedia(
-    language='en',
-    extract_format=wikipediaapi.ExtractFormat.WIKI,
-    user_agent='Jarvis/1.0'
-)
+API_KEY = 'AIzaSyB0hJ-RMNEA0TtwYezBsZ6cJpU07GbwGuM'
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-
-def search_wikipedia(query):
-    page = wiki_wiki.page(query)
-    if page.exists():
-        return page.summary[:300]  
+def search_google_knowledge_graph(query):
+    service = build("kgsearch", "v1", developerKey=API_KEY)
+    response = service.entities().search(query=query, limit=1).execute()
+    if 'itemListElement' in response:
+        result = response['itemListElement'][0]['result']
+        name = result.get('name', 'No name available')
+        description = result.get('description', 'No description available')
+        detailed_description = result.get('detailedDescription', {}).get('articleBody', 'No detailed description available')
+        print(query)
+        print(f"{name}: {description}. {detailed_description[:1000]}")
+        return f"{name}: {description}. {detailed_description[:1000]}"
     else:
         return "Sorry, I couldn't find any information on that topic."
 
@@ -41,32 +43,26 @@ def processCommand(c):
         else:
             speak("music not found")
     else:
-        response = search_wikipedia(command)
+        response = search_google_knowledge_graph(command)
         speak(response)
-    
 
 if __name__ == "__main__":
-    speak("Initializing Jarvis....")
+    speak("Hey I am Jarvis, your voice assistant.")
     while True:
         r = sr.Recognizer()
         try:
             with sr.Microphone() as source:
                 r.adjust_for_ambient_noise(source)
-                audio = r.listen(source,timeout=2)
+                audio = r.listen(source, timeout=2)
             word = r.recognize_google(audio)
             if word.lower() == "jarvis":
-                speak("Yes how can i help you?")
+                speak("Yes, how can I help you?")
                 with sr.Microphone() as source:
                     print("Jarvis Active...")
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
-
                     processCommand(command)
-
-
         except sr.UnknownValueError:
             print("Could not understand audio")
         except Exception as e:
             print(f"Error: {e}")
-    
-
